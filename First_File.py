a1df21b6f448e806bbd6961f1b118dc2a9253d6c
Airaj Firdaws <airajfirdaws@Airajs-MacBook-Air.local>
2024-11-27 13:34:08 +0100

    First upload

---
+++ First_File.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# file_name.py
+# Python 3.8
+"""
+Created on Wed Nov 27 13:32:51 2024
+
+@author: airajfirdaws
+Created: Wed Nov 27 13:32:51 2024
+Modified: Wed Nov 27 13:32:51 2024
+
+Description
+-----------
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#This is the sample file
